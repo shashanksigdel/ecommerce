5eb196555e4ee329218040735a29f2688a7261b6
shashanksigdel <sashanksigdel@gmail.com>
2021-11-09 12:46:23 +0545

    Comment Connection.js

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connectDB = async () => {
   try {
-    //mongodb conenction string
+    //mongodb conenction string connected
     const con = await mongoose.connect(process.env.LOCAL_DB_URI)
     console.log(`Database connected ${con.connection.host}`)
   } catch (err) {
